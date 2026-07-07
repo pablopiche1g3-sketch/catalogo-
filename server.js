eb6924695a54496ea646840c4f954f836c62be58
unknown <pablo@example.com>
2026-07-07 13:10:37 -0600

    Add server for Firebase App Hosting

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
